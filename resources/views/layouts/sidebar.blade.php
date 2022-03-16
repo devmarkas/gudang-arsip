d7588138960b4e4cc028bdab05c24d4f55712741
Arza Rizky <developer@markashosting.com>
2022-03-16 18:06:09 +0700

    update sidebar new menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,7 +7,6 @@
     </a>
 
    
-
     <div class="item-wrap">
     <div class="sidebar-title sidebar-heading mb-3 mt-4">
         <span>MENU</span>
@@ -15,40 +14,82 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="sb nav-item">
+
+        <!-- Nav Item - Dashboard -->
         <a class="nav-link {{ Request::is('/*') ? 'nav-active-custom' : '' }} " href="{{ route('home') }}" id="dashboard">
             @if (Request::is('/*'))
                 <img src="{{asset ("template")}}/img/icon-sidebar-dashboard-active.svg" alt="">
             @else
                 <img src="{{asset ("template")}}/img/icon-sidebar-dashboard.svg" alt="">
             @endif
-            <span class="{{ Request::is('/*') ? 'nav-active-custom-span' : '' }}">Dashboard</span></a>
-    </li>
-
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="sb nav-item">
-        <a class="nav-link collapsed active" href="#" data-toggle="collapse" data-target="#collapseTwo"
-            aria-expanded="true" aria-controls="collapseTwo">
-            <img src="{{asset ("template")}}/img/icon-sidebar-kelola-arsip.svg" alt="">
-            <span>Kelola Arsip</span>
+            <span class="{{ Request::is('/*') ? 'nav-active-custom-span' : '' }}">Dashboard</span>
         </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+
+        <!-- Nav Item - Pages Collapse Unit -->
+        <a class="nav-link collapsed active" href="#" data-toggle="collapse" data-target="#unit"
+            aria-expanded="true" aria-controls="collapseTwo">
+            <img class="nav-icon-impress" src="{{asset ("template")}}/img/icon-sidebar-kelola-arsip.svg" alt="">
+            <span>Unit</span>
+        </a>
+        <div id="unit" class="collapse show" aria-labelledby="headingTwo" data-parent="#unit">
             <div class="py-2 collapse-inner rounded">
-                <a class="nav-link {{ Request::is('impress-fund*') ? 'nav-active-custom' : '' }} " href="{{ route('impress_fund.index') }}" id="impress-fund">
-                @if (Request::is('impress-fund*'))
-                    <img src="{{asset ("template")}}/img/icon-sidebar-kelola-arsip-active.svg" alt="">
-                @else
-                    <img src="{{asset ("template")}}/img/icon-sidebar-kelola-arsip.svg" alt="">
-                @endif
-                <span class="{{ Request::is('impress-fund*') ? 'nav-active-custom-span' : '' }}">IMPRESS FUND</span>
+
+                <!-- Nav Item - Pages Collapse Menu Finance & Billco -->
+                <a class="nav-link collapsed active" href="#" data-toggle="collapse" data-target="#finance-billco"
+                    aria-expanded="true" aria-controls="collapseTwo">
+                    <img class="nav-icon-impress" src="{{asset ("template")}}/img/icon-sidebar-kelola-arsip.svg" alt="">
+                    <span>Finance & Billco</span>
                 </a>
-                <a class="nav-link {{ Request::is('tag-mitra*') ? 'nav-active-custom' : '' }} " href="{{ route('tag_mitra.index') }}" id="tag-mitra">
-                @if (Request::is('tag-mitra*'))
-                    <img src="{{asset ("template")}}/img/icon-sidebar-kelola-arsip-active.svg" alt="">
-                @else
-                    <img src="{{asset ("template")}}/img/icon-sidebar-kelola-arsip.svg" alt="">
-                @endif
-                <span class="{{ Request::is('tag-mitra*') ? 'nav-active-custom-span' : '' }}">TAG MITRA</span>
-                </a>    
+                <div id="finance-billco" class="collapse show" aria-labelledby="headingTwo" data-parent="#finance-billco">
+                    <div class="py-2 collapse-inner rounded">
+
+                        <!-- Nav Item - IMPRESS FUND -->
+                        <a class="nav-link {{ Request::is('impress-fund*') ? 'nav-active-custom' : '' }} " href="{{ route('impress_fund.index') }}" id="impress-fund">
+                            @if (Request::is('impress-fund*'))
+                                <img class="nav-icon-impress" src="{{asset ("template")}}/img/icon-sidebar-kelola-arsip-active.svg" alt="">
+                            @else
+                                <img class="nav-icon-impress" src="{{asset ("template")}}/img/icon-sidebar-kelola-arsip.svg" alt="">
+                            @endif
+                            <span class="{{ Request::is('impress-fund*') ? 'nav-active-custom-span' : '' }}">IMPRESS FUND</span>
+                        </a>
+
+                        <!-- Nav Item - TAG MITRA -->
+                        <a class="nav-link {{ Request::is('tag-mitra*') ? 'nav-active-custom' : '' }} " href="{{ route('tag_mitra.index') }}" id="tag-mitra">
+                            @if (Request::is('tag-mitra*'))
+                                <img src="{{asset ("template")}}/img/icon-sidebar-kelola-arsip-active.svg" alt="">
+                            @else
+                                <img src="{{asset ("template")}}/img/icon-sidebar-kelola-arsip.svg" alt="">
+                            @endif
+                            <span class="{{ Request::is('tag-mitra*') ? 'nav-active-custom-span' : '' }}">TAG MITRA</span>
+                        </a>    
+                                                
+                        <!-- Nav Item - BILLING COLLECTION -->
+                        <a class="nav-link" href="#" id="billing collection">
+                            <img class="nav-icon-impress" src="{{asset ("template")}}/img/icon-sidebar-kelola-arsip.svg" alt="">
+                            <span>BILLING COLLECTION</span>
+                        </a>
+                    </div>
+                </div>
+
+
+                <!-- Nav Item - Commerce -->
+                <a class="nav-link" href="#" id="commerce">
+                    <img class="nav-icon-impress" src="{{asset ("template")}}/img/icon-sidebar-kelola-arsip.svg" alt="">
+                    <span>Commerce</span>
+                </a>
+
+                <!-- Nav Item - Construction -->
+                <a class="nav-link" href="#" id="construction">
+                    <img class="nav-icon-impress" src="{{asset ("template")}}/img/icon-sidebar-kelola-arsip.svg" alt="">
+                    <span>Construction</span>
+                </a>
+
+                <!-- Nav Item - HCM -->
+                <a class="nav-link" href="#" id="hcm">
+                    <img class="nav-icon-impress" src="{{asset ("template")}}/img/icon-sidebar-kelola-arsip.svg" alt="">
+                    <span>HCM</span>
+                </a>
+
             </div>
         </div>
     </li>
--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -15,9 +15,8 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img class="img-profile rounded-circle"
-                    src="{{asset ("template")}}/img/undraw_profile.svg">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth::user()->name}}</span>
+                <img class="img-profile rounded-circle" src="{{asset ("template")}}/img/undraw_profile.svg">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small topbar-name">{{Auth::user()->name}}</span>
                 
             </a>
             <!-- Dropdown - User Information -->
